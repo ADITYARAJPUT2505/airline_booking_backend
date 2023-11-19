bdf389189f1167ac07244aacc6381bf38600d7ab
Aditya <ar9412349716@gmail.com>
2023-11-20 00:07:03 +0530

    db created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const bodyparser = require('body-parser')
 const{PORT} = require('./config/server_config')
 const setupAndStartServer= async()=>{
      const app = express();
+
+     app.use(bodyparser.json());
+     app.use(bodyparser.urlencoded({extended:true}));
+     
      app.listen(PORT,()=>{
       console.log(`server started at ${PORT}`)
      })
