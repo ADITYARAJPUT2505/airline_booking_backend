d0b305073f75d7487a4123ed0cd19c87cbb87f1c
Aditya <ar9412349716@gmail.com>
2023-11-20 22:32:04 +0530

    routes added

--- src/index.js
+++ src/index.js
@@ -8,11 +8,15 @@ const{PORT} = require('./config/server_config')
 
 //const CityRepository =require('./repository/city_repository')
 
+const ApiRoutes = require('./routes/index')
+
 const setupAndStartServer= async()=>{
      const app = express();
 
      app.use(bodyparser.json());
      app.use(bodyparser.urlencoded({extended:true}));
+
+     app.use('/api',ApiRoutes)
      
      app.listen(PORT,async()=>{
       console.log(`server started at ${PORT}`)
---
+++ src/routes/v1/index.js
@@ -0,0 +1,9 @@
+const express =require('express')
+
+const Citycontroller = require('../../controllers/city_controllers')
+
+const router=express.Router();
+
+router.post('/city',Citycontroller.create)
+
+module.exports = router
