86d2fb4686af1af8f0c0f7d448bbc71fe93a63da
Aditya <ar9412349716@gmail.com>
2023-11-20 20:54:03 +0530

    controller added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+
 const bodyparser = require('body-parser')
-const {City} = require('./models/index')
+
+//const {City} = require('./models/index')
+
 const{PORT} = require('./config/server_config')
-const CityRepository =require('./repository/city_repository')
+
+//const CityRepository =require('./repository/city_repository')
+
 const setupAndStartServer= async()=>{
      const app = express();
 
@@ -11,8 +16,8 @@ const setupAndStartServer= async()=>{
      
      app.listen(PORT,async()=>{
       console.log(`server started at ${PORT}`)
-      const repo = new CityRepository();
-      await repo.CreateCity({name:"mumbai"})
+     //  const repo = new CityRepository();
+     //  await repo.CreateCity({name:"mumbai"})
     //   City.create({
     //       name:"delhi",
     //   }).then((data)=>{
